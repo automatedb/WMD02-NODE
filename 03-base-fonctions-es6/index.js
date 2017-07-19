3faf2fd3d43825e594287b5a871e58162f84e330
automatedb <contact@robots-trading.com>
2017-07-19 10:32:20 +0200

    Initial commit

---
+++ 03-base-fonctions-es6/index.js
@@ -0,0 +1,13 @@
+// const add = function(a, b) {
+//     return a + b;
+// };
+
+// const add = (a, b) => { return a + b; };
+
+// const add = (a, b) => a + b;
+//
+// console.log(add(1, 1));
+
+const multiply2 = a => a * 2;
+
+console.log(multiply2(1));
