a5f79cd66f9a6e3b15cfd75c39a95304d0edf731
automatedb <contact@robots-trading.com>
2017-07-19 12:02:10 +0200

    Initial commit

--- 06-exports/index.js
+++ 06-exports/index.js
@@ -1,5 +1,7 @@
-const Bibi = require('./Bibi');
+const Bibi = require('./classes/Bibi');
+const Config = require('./config.json');
 
 const bibi = new Bibi();
 
 console.log(bibi.getName());
+console.log(Config);
